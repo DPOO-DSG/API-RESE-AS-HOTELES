6f69fc0bf85c5ef5890fc2ebe6d5b4866de676cf
Daniel Santiago Acosta Gómez <ds.acosta@uniandes.edu.co>
2026-05-24 18:57:58 -0500

    espacio

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS 
+from flask_cors import CORS
 from pymongo import MongoClient
 from bson import ObjectId
 from datetime import datetime
